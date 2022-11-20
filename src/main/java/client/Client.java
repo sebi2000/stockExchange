d5b848918ef8dab6e849003cd662e638a41dfa5f
ραρεσ <57955143+StoicaRares14@users.noreply.github.com>
2022-11-20 13:21:56 +0200

    Make client start a new thread when reading from console

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,23 +1,40 @@
 package client;
 
-import java.net.*;
+import server.Server;
+import utils.Helpers;
+
 import java.io.*;
-class Client{
-    public static void main(String args[])throws Exception{
-        Socket s=new Socket("localhost",3333);
-        DataInputStream din=new DataInputStream(s.getInputStream());
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+import java.util.UUID;
 
-        String str="",str2="";
-        while(!str.equals("stop")){
-            str=br.readLine();
-            dout.writeUTF(str);
-            dout.flush();
-            str2=din.readUTF();
-            System.out.println("Server says: "+str2);
+class Client {
+    public static void main(String args[]) throws Exception {
+        UUID userId = UUID.randomUUID();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = "";
+        while (!str.equals("stop")) {
+            str = br.readLine();
+
+            String[] input = str.split(" ");
+
+            if (input.length > 1) {
+                String operation = input[0].toLowerCase();
+                String amount = input[1];
+
+                if (operation.equals("buy") || operation.equals("sell")) {
+                    if (!Helpers.isNumeric(amount)) {
+                        System.out.println(userId + ": " + "Input needs to be 'sell'/'buy' <number>");
+                    } else {
+                        Server s = new Server();
+                        s.getUserId(userId);
+                        s.getOperation(operation);
+                        s.getAmount(Integer.parseInt(amount));
+                        s.start();
+                        s.join();
+                    }
+                }
+            }
         }
+    }
+}
 
-        dout.close();
-        s.close();
-    }}
